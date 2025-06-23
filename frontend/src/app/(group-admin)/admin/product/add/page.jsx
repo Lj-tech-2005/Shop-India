d4623f8552a36b75e766c79281075989c73f8bbd
Lj-tech-2005 <lokeshrjj2005@gmail.com>
2025-06-23 16:17:36 +0530

    e

--- frontend/src/app/(group-admin)/admin/product/add/page.jsx
+++ frontend/src/app/(group-admin)/admin/product/add/page.jsx
@@ -1,4 +1,5 @@
 'use client';
+export const dynamic = 'force-dynamic';
 import { useEffect, useRef, useState } from 'react';
 import { FaPlus } from 'react-icons/fa';
 import { axiosApiInstance, createSlug, notify } from '@/app/library/helper';
